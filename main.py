46fe2ba1986a867d5631f0db08dbf33935337340
altUSER <blaksteam@yandex.ru>
2018-10-04 23:27:47 +0500

    Fix bug with img. Finish rst func. Del some bugs.

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ def rst(event):
 		fname=str(time.strftime('%Y_%b_%d_%H:%M:%S')) + '.conf'
 		os.system('cp class.conf log/' + fname)
 		write_msg(admin_id, '[AUTO INFO]Произведен сброс по запросу ' + str(event.peer_id) + ', файл сохранен как log/' + fname)
+		for id in conf.sections():
+			conf.set(str(id), 'eat', '-')
 		return('Произведен сброс, файл сохранен как log/' + fname)
 	else:
 		return('Ошибка! Директория log/ не обнаружена.')
@@ -29,47 +31,47 @@ def geteatlist(conf):
 
 	longpoll = vk_api.longpoll.VkLongPoll(vk)
 	for event in longpoll.listen(): #ловля сообщений
+		time.sleep(0.5)
 
 		if event.type == VkEventType.MESSAGE_NEW: 
 			msg = event.text
 			inp = msg.split()
 
+			if (len(inp) != 0):
+				if (inp[0] == 'eat') or (inp[0] == 'Eat'):
 
-			if inp[0] == 'eat':
+					conf_file = open('class.conf', 'w') #имя конфиг файла
 
-				conf_file = open('class.conf', 'w') #имя конфиг файла
+					if inp[1] == 'y':
+						old_v = conf.get(str(event.peer_id), 'eat')
+						conf.set(str(event.peer_id), 'eat', 'y')
+						write_msg(event.peer_id, old_v + ' заменено на y')
+					if inp[1] == 'n':
+						old_v = conf.get(str(event.peer_id), 'eat')
+						conf.set(str(event.peer_id), 'eat', 'n')
+						write_msg(event.peer_id, old_v + ' заменено на n')
 
-				if inp[1] == 'y':
-					old_v = conf.get(str(event.peer_id), 'eat')
-					conf.set(str(event.peer_id), 'eat', 'y')
-					write_msg(event.peer_id, old_v + ' заменено на y')
-				if inp[1] == 'n':
-					old_v = conf.get(str(event.peer_id), 'eat')
-                                        conf.set(str(event.peer_id), 'eat', 'n')
-                                        write_msg(event.peer_id, old_v + ' заменено на n')
+					if inp[1] == 'list':
+						eat_y = 'Едят:\n'
+						y_count = 0
+						eat_n = '\nНе едят:\n'
+						ncheck = '\nНе отметелись:\n'
+						for id in conf.sections():
+							if conf.get(id, 'eat') == '-':
+								ncheck += conf.get(id, 'name') + '\n'
+							if conf.get(id, 'eat') == 'y':
+								y_count += 1
+								eat_y += conf.get(id, 'name') + '\n'
+							if conf.get(id, 'eat') == 'n':
+								eat_n += conf.get(id, 'name') + '\n'
 
-				if inp[1] == 'list':
-					eat_y = 'Едят:\n'
-					y_count = 0
-					eat_n = '\nНе едят:\n'
-					ncheck = '\nНе отметелись:\n'
-					for id in conf.sections():
-						if conf.get(id, 'eat') == '-':
-							ncheck += conf.get(id, 'name') + '\n'
-						if conf.get(id, 'eat') == 'y':
-							y_count += 1
-							eat_y += conf.get(id, 'name') + '\n'
-						if conf.get(id, 'eat') == 'n':
-							eat_n += conf.get(id, 'name') + '\n'
+						write_msg(event.peer_id, eat_y + 'Всего: ' + str(y_count) + '\n' + eat_n + ncheck)
 
-					write_msg(event.peer_id, eat_y + 'Всего: ' + str(y_count) + '\n' + eat_n + ncheck)
+					if inp[1] == 'reset':
+						write_msg(event.peer_id, rst(event))
 
-				if inp[1] == 'reset':
-					write_msg(event.peer_id, rst(event))
-
-
-				conf.write(conf_file)
-				conf_file.close()
+					conf.write(conf_file)
+					conf_file.close()
 
 
 vk = vk_api.VkApi(login = login, password = password) #переменные из vars
