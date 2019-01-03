d22ed2e41f76c23dd5506534062241ecad282956
altUSER <root@localhost.localdomain>
2019-01-04 00:09:07 +0500

    another stuff

--- main.py
+++ main.py
@@ -12,18 +12,40 @@ import time
 admin_id = 160946239
 
 def write_msg(id, s):
-    vk.method('messages.send', {'user_id':id,'message':s})
+    vk.method('messages.send', {'user_id':id,'message':s,'random_id':(int(time.strftime('%H')) + int(time.strftime('%M')) + int(time.strftime('%S')))})
 
-def rst(event):
-	if os.path.isdir('log'):
-		fname=str(time.strftime('%Y_%b_%d_%H:%M:%S')) + '.conf'
-		os.system('cp class.conf log/' + fname)
-		write_msg(admin_id, '[AUTO INFO]Произведен сброс, файл сохранен как log/' + fname)
-		for id in conf.sections():
-			conf.set(str(id), 'eat', '-')
-		return('Произведен сброс, файл сохранен как log/' + fname)
+def cfg_set(sect, set, val):
+	conf_file = open('class.conf', 'w') #имя конфиг файла
+
+	conf.set(str(sect), str(set), str(val))
+
+	conf.write(conf_file)
+	conf_file.close()
+
+def save(event):
+	if os.path.isdir('save'):
+		fname=str(time.strftime('%Y_%m_%d'))
+		os.system('cp class.conf save/' + fname)
+		write_msg(admin_id, '[AUTO INFO]Сохранение БД, файл сохранен как save/' + fname)
+		return('Сохранение БД, файл сохранен как ' + fname)
 	else:
-		return('Ошибка! Директория log/ не обнаружена.')
+		return('Ошибка! Директория save/ не обнаружена.')
+
+def list(conf):
+	eat_y = 'Едят:\n'
+	y_count = 0
+	eat_n = '\nНе едят:\n'
+	n_check ='\nНе отметились:\n'
+	for id in conf.sections():
+		if conf.get(id, 'eat') == 'y':
+			y_count += 1
+			eat_y += conf.get(id, 'name') + '\n'
+		if conf.get(id, 'eat') == 'n':
+			eat_n += conf.get(id, 'name') + '\n'
+		if conf.get(id, 'eat') == '-':
+			n_check += conf.get(id, 'name') + '\n'
+
+	return(eat_y + 'Всего: ' + str(y_count) + '\n' + eat_n + '\n' + n_check)
 
 def geteatlist(conf):
 
@@ -40,35 +62,39 @@ def geteatlist(conf):
 			if (len(inp) != 0):
 				if (inp[0] == 'eat'):
 
-					conf_file = open('class.conf', 'w') #имя конфиг файла
-
-					if inp[1] == 'y' and len(inp) == 2 and event.peer_id in conf.sections(): #отметка на акк отправляющего
+					if inp[1] == 'y' and len(inp) == 2 and (str(event.peer_id) in conf.sections()): #отметка на акк отправляющего
 						old_v = conf.get(str(event.peer_id), 'eat')
 						conf.set(str(event.peer_id), 'eat', 'y')
 						write_msg(event.peer_id, old_v + ' заменено на y')
-					if inp[1] == 'n' and len(inp) == 2 and event.peer_id in conf.sections():
+					if inp[1] == 'n' and len(inp) == 2 and (str(event.peer_id) in conf.sections()):
 						old_v = conf.get(str(event.peer_id), 'eat')
 						conf.set(str(event.peer_id), 'eat', 'n')
 						write_msg(event.peer_id, old_v + ' заменено на n')
 
 					if inp[1] == 'list' and len(inp) == 2: #вывод всех данных
-						eat_y = 'Едят:\n'
-						y_count = 0
-						eat_n = '\nНе едят:\n'
-						ncheck = '\nНе отметелись:\n'
-						for id in conf.sections():
-							if conf.get(id, 'eat') == '-':
-								ncheck += conf.get(id, 'name') + '\n'
-							if conf.get(id, 'eat') == 'y':
-								y_count += 1
-								eat_y += conf.get(id, 'name') + '\n'
-							if conf.get(id, 'eat') == 'n':
-								eat_n += conf.get(id, 'name') + '\n'
 
-						write_msg(event.peer_id, eat_y + 'Всего: ' + str(y_count) + '\n' + eat_n + ncheck)
+						write_msg(event.peer_id, list(conf))
 
-					if inp[1] == 'reset' and len(inp) == 2: #сброс значений
-						write_msg(event.peer_id, rst(event))
+					if inp[1] == 'save' and len(inp) == 2: #сохранение значений
+						write_msg(event.peer_id, save(event))
+
+					if inp[1] == 'archive' and len(inp) == 3:
+
+						if inp[2] == 'list':
+							outp = ''
+							c = 0
+							for n in os.listdir('save'):
+								outp += n + '(' + str(c) + ')' + '\n'
+								c += 1
+							write_msg(event.peer_id, outp)
+						else:
+							try:
+								cfg = cp.RawConfigParser()
+								cfg.read('save/' + os.listdir('save')[int(inp[2])]) #имя конфиг файла
+								outp = 'Архив на ' + os.listdir('save')[int(inp[2])] + ':\n' + list(cfg)
+							except Exception as e:
+								outp = 'Ошибка ' + str(e)
+							write_msg(event.peer_id, outp)
 
 					if inp[1] == 'am' and len(inp) == 4: #отметка другого человека
 						try:
@@ -78,18 +104,15 @@ def geteatlist(conf):
 
 							if inp[3] == 'y':
 								old_v = conf.get(id, 'eat')
-								conf.set(id, 'eat', 'y')
+								cfg_set(id, 'eat', 'y')
 								write_msg(event.peer_id, old_v + ' заменено на y')
 
 							if inp[3] == 'n':
 								old_v = conf.get(id, 'eat')
-								conf.set(id, 'eat', 'n')
+								cfg_set(id, 'eat', 'n')
 								write_msg(event.peer_id, old_v + ' заменено на n')
 						except:
 							write_msg(event.peer_id, 'Не а')
-
-					conf.write(conf_file)
-					conf_file.close()
 
 while True:
 	try:
